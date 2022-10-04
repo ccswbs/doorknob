3985d5abab9f001c7a76a64ead5e4f4e675aa870
Miranda Mafe <miranda.mafe@gmail.com>
2022-10-04 16:51:33 -0400

    Add Wordpress schema

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,39 @@
 const path = require("path")
 
+
+exports.createSchemaCustomization = ({ actions, schema }) => {
+
+  const { createTypes } = actions
+
+  const typeDefs = [
+    `
+      type WpEventToEventsCategoryConnection implements Node {
+        nodes: [WpEventsCategory]
+      }
+      type WpEventsCategory implements Node {
+        name: String
+      }
+    `,
+
+    schema.buildObjectType({
+      name: `WpEvent`,
+      interfaces: [`Node`],
+      fields: {
+        endDate: `String`,
+        startDate: `String`,
+        title: `String`,
+        url: `String`,
+        eventsCategories: `WpEventToEventsCategoryConnection`,
+        isPast: {
+          type: `Boolean`,
+          resolve: (source) => new Date(source.startDate) < new Date(),
+        },
+      },
+    }),
+  ]
+  createTypes(typeDefs)
+}
+
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions;
 
