6d761d35cf9086b3677e6bbbde6a07f5eb98d153
Miranda Mafe <miranda.mafe@gmail.com>
2022-10-14 14:35:24 -0400

    Add modal video

--- src/components/blocks/home/homeStory.js
+++ src/components/blocks/home/homeStory.js
@@ -4,6 +4,7 @@ import styled from "styled-components"
 import Overlay from "../../shared/overlay"
 import { getImage, GatsbyImage } from "gatsby-plugin-image"
 import { Col, Container, Row } from "react-bootstrap"
+import ModalVideo from "../../shared/modalVideo"
 
 const QuoteMark = styled.i`
     color: var(--bs-yellow);
@@ -17,26 +18,42 @@ const render = ({ content, background }) => {
           alt={background.alt} 
           className="opacity-75 h-100">
         <Container>
-          <Row className="bg-transparent h-100 text-white pb-0">
-            <Col lg={6} className="mb-4 d-flex flex-column justify-content-center">
-              <blockquote className="display-3 text-white text-center">
+          <Row className="bg-transparent h-100 text-white pb-0 px-5 pt-5">
+
+            <Col xl={6} className="mb-4 d-flex flex-column justify-content-center">
+              <blockquote className="display-4 text-white text-center">
                 <QuoteMark className="fa-solid fa-quote-left pe-2" aria-hidden="true" /> 
                   <em>{content.blockquote}</em>
                 <QuoteMark className="fa-solid fa-quote-right ps-2" aria-hidden="true" />
               </blockquote>
             </Col>
 
-            <Col lg={6} className="d-flex justify-content-center">
+            <Col xl={6} className="d-flex justify-content-center" style={{ marginTop: "-4rem" }}>
               <GatsbyImage image={getImage(content.foreground.src)} alt={content.foreground.alt} className="align-self-end img-fluid" />
             </Col>
           </Row>
         </Container>
       </Overlay.GatsbyImage>
-
-      <Col lg={12} className="align-items-center d-flex fs-4 justify-content-center p-3 text-white">
-        <p className="mb-0">{content.cta}</p>
-        <button type="button" className="btn btn-primary ms-4 my-4"><i className="fa-solid fa-play me-2" /> Watch Video</button>
-      </Col>
+      <div className="bg-black">
+        <Container>
+          <Row>
+            <Col lg={12} className="align-items-center d-flex fs-4 justify-content-center p-3 text-white">
+              <p className="mb-0">{content.cta}</p>
+              <ModalVideo 
+                id={content.video.id} 
+                src={content.video.url} 
+                title={content.video.title} 
+                transcript={content.video.transcript} 
+                modalButton = {
+                    <button type="button" className="btn btn-primary ms-4 my-4">
+                        <i className="fa-solid fa-play me-2" /> Watch Video<span className="visually-hidden">: {content.video.title}</span>
+                    </button>
+                }
+              />
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </Row>
   )
 }
@@ -51,20 +68,28 @@ const query = graphql`
           src {
             childImageSharp {
               gatsbyImageData (
-                height: 600
+                height: 390
               )
             }
           }
           alt
         }
         cta
+        video {
+          id
+          type
+          title
+          url
+          transcript
+          captions
+        }
       }
       background {
         src {
           childImageSharp {
             gatsbyImageData (
               width: 1400,
-              height: 340,
+              height: 300,
             )
           }
         }
