8634ddec157a57a42f8edfaabca2e44537779fd8
Miranda Mafe <miranda.mafe@gmail.com>
2022-10-17 16:48:55 -0400

    Update links spacing

--- src/components/blocks/home/homeLinks.js
+++ src/components/blocks/home/homeLinks.js
@@ -5,7 +5,7 @@ import { Container } from "react-bootstrap"
 const render = ({ links }) => {
   return (
     <Container className="content-block">
-      <div className="d-grid d-md-block gap-2">
+      <div className="d-grid d-md-block gap-3">
         { links.map((link, index) => {
           return <a href={link.url} key={`homelinks-${index}`} className="btn btn-lg btn-outline-info mb-md-3 me-md-3 p-4 text-start">{link.title}</a>
         })}
