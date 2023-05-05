16fcd2f6f685862057126dd2619d8733cc8ece9d
Tanya Qureshi <tqureshi@uoguelph.ca>
2023-05-05 15:12:25 -0400

    Add text overlay divs

--- src/components/blocks/home/homeHero.js
+++ src/components/blocks/home/homeHero.js
@@ -5,10 +5,25 @@ import { Row } from "react-bootstrap"
 
 const render = ({ image }) => {
   return (
-    <Row id="rotator" className="content-block">
-        <GatsbyImage style={{ gridArea: "1/1" }} image={getImage(image.src)} alt={image.alt} />
+    <Row id="rotator" className="mb-md-5 position-relative">
+        <GatsbyImage image={getImage(image.src)} alt={image.alt} />
+        <div className="d-flex p-0 position-absolute h-100 top-50 start-50 translate-middle" style={{ maxWidth:"1320px" }}>
+            {/* <div className="me-auto align-self-center bg-dark bg-opacity-75 col-md-4 p-4 p-lg-5 text-white">
+                <h1 className="h3 text-white">Top Center Start</h1>
+                <p>Left-aligned, vertically centered</p>
+            </div>
+            
+            <div className="ms-auto align-self-center bg-dark bg-opacity-75 col-md-4 p-4 p-lg-5 text-white">
+                <h1 className="h3 text-white">Top Center End</h1>
+                <p>Right-aligned, vertically centered</p>
+            </div> */}
+            <div className="align-self-center bg-dark bg-opacity-75 w-100 p-4 p-lg-5 mb-lg-5 mt-auto text-center text-white">
+                <h1 className="h3 text-white">Bottom Center</h1>
+                <p>Horizontally centered on the baseline with space underneath on large displays</p>
+            </div>
+        </div>
     </Row>
-  )
+   )
 }
 
 const query = graphql`
