c97c5530fd0c75b3f0ff68492b6161a2eb8d83af
Raza Mahmood <razammahmood@gmail.com>
2023-07-11 14:29:19 -0400

    make sure alert is first element in the dom

--- src/components/appArmorAlert.js
+++ src/components/appArmorAlert.js
@@ -1,13 +1,25 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Helmet } from "react-helmet"
 
-export default ({ id }) => {
+const AppArmorAlert = ({ id }) => {
+  useEffect(() => {
+    const appArmorID = "AppArmorAlertID_" + id
+
+    // Remove any existing AppArmorAlerts with the same ID to prevent duplicates
+    document.querySelectorAll("#" + appArmorID).forEach((e) => e.remove());
+
+    const container = document.createElement("div")
+    container.id = appArmorID
+    document.body.prepend(container)
+  }, [id])
+
   return (
     <>
-      <div id={"AppArmorAlertID_" + id}></div>
       <Helmet>
         <script src={"https://uoguelph.apparmor.com/Notifications/Feeds/Javascript/?AlertID=" + id}></script>
       </Helmet>
     </>
   )
 }
+
+export default AppArmorAlert
