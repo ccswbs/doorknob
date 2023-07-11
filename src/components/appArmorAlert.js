3a63ebae7c1c442e48b366c9b9aea90d1197aad4
Raza Mahmood <razammahmood@gmail.com>
2023-07-11 14:52:08 -0400

    ensure aaa script loads after container creation

--- src/components/appArmorAlert.js
+++ src/components/appArmorAlert.js
@@ -1,25 +1,23 @@
 import React, { useEffect } from "react"
-import { Helmet } from "react-helmet"
 
 const AppArmorAlert = ({ id }) => {
   useEffect(() => {
     const appArmorID = "AppArmorAlertID_" + id
 
     // Remove any existing AppArmorAlerts with the same ID to prevent duplicates
-    document.querySelectorAll("#" + appArmorID).forEach((e) => e.remove());
+    document.querySelectorAll("#" + appArmorID).forEach(e => e.remove())
 
     const container = document.createElement("div")
     container.id = appArmorID
     document.body.prepend(container)
+
+    // Need to load the script after the container is added to the DOM, otherwise the app armor script will not find the container
+    const script = document.createElement("script")
+    script.src = "https://uoguelph.apparmor.com/Notifications/Feeds/Javascript/?AlertID=" + id
+    container.appendChild(script)
   }, [id])
 
-  return (
-    <>
-      <Helmet>
-        <script src={"https://uoguelph.apparmor.com/Notifications/Feeds/Javascript/?AlertID=" + id}></script>
-      </Helmet>
-    </>
-  )
+  return <></>
 }
 
 export default AppArmorAlert
