d07e4a2ec5fe17bf89ad95b87b8e83f24c33bf38
Raza Mahmood <razammahmood@gmail.com>
2023-07-26 15:40:18 -0400

    adding placeholder images to pathway links

--- src/components/blocks/home/homeLinksPrimary.js
+++ src/components/blocks/home/homeLinksPrimary.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { Container } from "react-bootstrap"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 const query = graphql`
   query {
@@ -9,6 +10,14 @@ const query = graphql`
       links {
         title
         url
+        image {
+          src {
+            childImageSharp {
+              gatsbyImageData(width: 300)
+            }
+          }
+          alt
+        }
       }
     }
   }
@@ -23,8 +32,12 @@ export default function HomeLinksPrimary() {
 
       <ul className="list-unstyled row g-md-5 g-3">
         {links.map(link => (
-          <li key={link.url} className="col-md-4">
-            <a className="btn btn-dark w-100 d-flex align-items-center justify-content-center" href={link.url}>
+          <li key={link.url} className="col-md-4 position-relative">
+            <GatsbyImage image={getImage(link.image.src)} alt={link.image.alt} className="w-100" />
+            <a
+              className="btn btn-dark w-100 d-flex align-items-center justify-content-center stretched-link"
+              href={link.url}
+            >
               <h3 className="fs-5 fw-normal">{link.title}</h3>
             </a>
           </li>
