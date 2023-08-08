5434cca84932b23e7f09115124a647490638d011
Raza Mahmood <razammahmood@gmail.com>
2023-08-08 15:37:41 -0400

    add margin top to tagline heading

--- src/components/blocks/home/homeTagline.js
+++ src/components/blocks/home/homeTagline.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export const HomeTagline = () => (
   <div className="container text-center">
-    <h2 className="mb-4">University of Guelph, Ontario, Canada</h2>
+    <h2 className="mt-5 mb-4">University of Guelph, Ontario, Canada</h2>
     <p className="text-secondary fs-5">We're on a mission to Improve Life at home and around the world</p>
     <hr />
   </div>
