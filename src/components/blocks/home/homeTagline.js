de65ec4124704013cd8b9fc5af721a8ad0bce6d8
Raza Mahmood <razammahmood@gmail.com>
2023-08-08 15:22:35 -0400

    Add tagline block

---
+++ src/components/blocks/home/homeTagline.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const HomeTagline = () => (
+  <div className="container text-center">
+    <h2 className="mb-4">University of Guelph, Ontario, Canada</h2>
+    <p className="text-secondary fs-5">We're on a mission to Improve Life at home and around the world</p>
+    <hr />
+  </div>
+)
+
+export default HomeTagline
