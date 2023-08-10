2ba2a7fa5fde3a3795a5eea7b33b536c447be5e9
Raza Mahmood <razammahmood@gmail.com>
2023-08-10 14:06:16 -0400

    apply alignment field to each individual card

--- src/components/blocks/home/homeCardsSpotlight.js
+++ src/components/blocks/home/homeCardsSpotlight.js
@@ -8,7 +8,6 @@ export default function HomeCardsSpotlight() {
   const spotlightData = useSpotlightData()
   const cardData = spotlightData.cards
   const quantity = cardData.length
-  const imageAlignment = spotlightData?.imageAlignment ?? "center" // 3 options: left, center, right
   const rowClasses = classNames(
     "row",
     "row-cols-1",
@@ -19,25 +18,6 @@ export default function HomeCardsSpotlight() {
       "row-cols-md-2 row-cols-xl-4": quantity === 4,
     }, // xl: allow 4 on a single row
   )
-  const cardClasses = classNames(
-    "h-100",
-    "border-0",
-    "bg-info",
-    "bg-opacity-10",
-    "spotlight-card",
-    { "left-align-image": imageAlignment === "left" },
-    { "right-align-image": imageAlignment === "right" },
-  )
-
-  const cardClasses = classNames(
-    "h-100",
-    "border-0",
-    "bg-info",
-    "bg-opacity-10",
-    "spotlight-card",
-    { "left-align-image": imageAlignment === "left" },
-    { "right-align-image": imageAlignment === "right" },
-  )
 
   return (
     quantity > 1 && (
@@ -45,6 +25,17 @@ export default function HomeCardsSpotlight() {
         <h2 className="mt-5 mb-5 text-primary">Our Latest News and Events</h2>
         <div className={rowClasses}>
           {cardData.map(item => {
+            const imageAlignment = item?.imageAlignment ?? "center" // 3 options: left, center, right
+            const cardClasses = classNames(
+              "h-100",
+              "border-0",
+              "bg-info",
+              "bg-opacity-10",
+              "spotlight-card",
+              { "left-align-image": imageAlignment === "left" },
+              { "right-align-image": imageAlignment === "right" },
+            )
+
             return (
               <Col key={item.key} className="mt-4 mb-4">
                 <Card className={cardClasses}>
