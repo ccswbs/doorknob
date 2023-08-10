19ac41dfda55a897897d1d4c8e7e393f1c514d6b
Raza Mahmood <razammahmood@gmail.com>
2023-08-10 16:43:54 -0400

    remove yellow border from tagline

--- src/components/blocks/home/homeTagline.js
+++ src/components/blocks/home/homeTagline.js
@@ -3,7 +3,7 @@ import React from "react"
 export const HomeTagline = () => (
   <div className="container text-center d-flex flex-column">
     <h2 className="mt-5 mb-4">University of Guelph, Ontario, Canada</h2>
-    <p className="lead text-dark border-start border-5 border-warning ps-4 display-1 fs-5 mx-auto">We're on a mission to Improve Life at home and around the world</p>
+    <p className="lead text-dark ps-4 display-1 fs-5 mx-auto">We're on a mission to Improve Life at home and around the world</p>
     <hr className="w-100"></hr>
   </div>
 )
