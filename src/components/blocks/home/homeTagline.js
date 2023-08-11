9430693aaf4f4e5c9ade0787c946713c4978b2f0
Raza Mahmood <razammahmood@gmail.com>
2023-08-11 16:03:40 -0400

    update hr in tagline

--- src/components/blocks/home/homeTagline.js
+++ src/components/blocks/home/homeTagline.js
@@ -4,7 +4,7 @@ export const HomeTagline = () => (
   <div className="container text-center d-flex flex-column">
     <h2 className="mt-5 mb-4">University of Guelph, Ontario, Canada</h2>
     <p className="lead text-dark ps-4 display-1 fs-5 mx-auto">We're on a mission to Improve Life at home and around the world</p>
-    <hr className="w-100"></hr>
+    <hr className="w-75 mx-auto"></hr>
   </div>
 )
 
