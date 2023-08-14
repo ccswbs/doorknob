2425476539c04f7871d3b809bffee0f9eb4163c3
Raza Mahmood <razammahmood@gmail.com>
2023-08-14 15:17:35 -0400

    remove unused imports and code

--- src/components/beRelatedLinks.js
+++ src/components/beRelatedLinks.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react"
+import React from "react"
 import { Helmet } from "react-helmet"
 
 const BERelatedLinks = () => {
   return (
     <>
-      <div className="be-ix-link-block" dangerouslySetInnerHTML={{__html: "<!--Link Block Target Div--></>"}}></div>
+      <div className="be-ix-link-block"></div>
       <Helmet>
         <script type="text/javascript" src="https://cdn.bc0a.com/autopilot/f00000000209359/autopilot_sdk.js"></script>
       </Helmet>
