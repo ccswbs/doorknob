62c6485109a57ccd4d22e40afd04016f96d44797
Raza Mahmood <razammahmood@gmail.com>
2023-08-25 13:27:51 -0400

    update tagline text

--- src/components/blocks/home/homeTagline.js
+++ src/components/blocks/home/homeTagline.js
@@ -3,7 +3,7 @@ import React from "react"
 export const HomeTagline = () => (
   <div className="container text-center d-flex flex-column home-tagline">
     <h2 className="mt-5 mb-4">University of Guelph, Ontario, Canada</h2>
-    <p className="lead text-dark ps-4 display-1 fs-5 mx-auto">Rooted in Excellence, connected by our Gryphon Spirit we aim to Improve Life.</p>
+    <p className="lead text-dark ps-4 display-1 fs-4 mx-auto">Improve Life</p>
     <hr className="w-75 mx-auto"></hr>
   </div>
 )
