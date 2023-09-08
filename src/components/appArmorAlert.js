0e91c2b10c745fb4c0a5690e9b147f3eb17d3c5e
Raza Mahmood <razammahmood@gmail.com>
2023-09-08 10:28:28 -0400

    using gatsby script rather than react helmet

--- src/components/appArmorAlert.js
+++ src/components/appArmorAlert.js
@@ -1,18 +1,14 @@
 import React from "react"
-import { Helmet } from "react-helmet"
+import { Script } from "gatsby"
 
 const AppArmorAlert = ({ id = "163" }) => {
   return (
     <>
       <div id={"AppArmorAlertID_" + id}></div>
-      <Helmet>
-        <script
-          id="AppArmorAlertScriptID"
-          type="text/javascript"
-          src={"https://uoguelph.apparmor.com/Notifications/Feeds/Javascript/?AlertID=" + id}
-          defer
-        ></script>
-      </Helmet>
+      <Script
+        src={"https://uoguelph.apparmor.com/Notifications/Feeds/Javascript/?AlertID=" + id}
+        strategy="post-hydrate"
+      ></Script>
     </>
   )
 }
--- src/components/beRelatedLinks.js
+++ src/components/beRelatedLinks.js
@@ -1,13 +1,11 @@
 import React from "react"
-import { Helmet } from "react-helmet"
+import { Script } from "gatsby"
 
 const BERelatedLinks = () => {
   return (
     <>
       <div className="be-ix-link-block"></div>
-      <Helmet>
-        <script type="text/javascript" src="https://cdn.bc0a.com/autopilot/f00000000209359/autopilot_sdk.js"></script>
-      </Helmet>
+      <Script src="https://cdn.bc0a.com/autopilot/f00000000209359/autopilot_sdk.js"></Script>
     </>
   )
 }
