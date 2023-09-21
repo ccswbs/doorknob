6b35ebacf67904cb4d92714631bbfaf841281f81
Raza Mahmood <razammahmood@gmail.com>
2023-09-21 10:20:25 -0400

    add filter hook

---
+++ src/hooks/use-filter.js
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+const DEFAULT_CONFIG = {
+  maxEditDistance: 0
+}
+
+const levenshtein = (a, b) => {
+  let distances = []
+
+  for (let i = 0; i <= a.length; i++) {
+    distances[i] = []
+
+    for (let j = 0; j <= b.length; j++) {
+      if (Math.min(i, j) == 0) {
+        distances[i][j] = Math.max(i, j)
+      } else {
+        if (a.charAt(i - 1) != b.charAt(j - 1)) {
+          distances[i][j] = Math.min(distances[i - 1][j] + 1, distances[i][j - 1] + 1, distances[i - 1][j - 1] + 1)
+        } else {
+          distances[i][j] = Math.min(distances[i - 1][j] + 1, distances[i][j - 1] + 1, distances[i - 1][j - 1])
+        }
+      }
+    }
+  }
+
+  return distances[a.length][b.length]
+}
+
+export const useFilter = (data, config = {}) => {}
--- src/pages/program-search.js
+++ src/pages/program-search.js
@@ -1,7 +1,12 @@
 import React from "react"
+import { Container } from "react-bootstrap";
 
 const ProgramSearch = () => {
-
+  return (
+    <Container>
+      <h1>Program Search</h1>
+    </Container>
+  )
 }
 
 export default ProgramSearch;
