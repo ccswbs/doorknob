aa6c96bf11c2aa0b526136dcdeec9aa45db08aa9
Raza Mahmood <razammahmood@gmail.com>
2023-10-25 16:03:13 -0400

    temporarily show tags in programCard.js

--- src/components/blocks/programs/programCard.js
+++ src/components/blocks/programs/programCard.js
@@ -1,9 +1,9 @@
 import { toTitleCase } from "../../../utils/toTitleCase"
 import React from "react"
 
-export const ProgramCard = ({ title, acronym, url = "#", degrees = [], types = [] }) => (
+export const ProgramCard = ({ title, acronym, url = "#", degrees = [], types = [], tags = []}) => (
   <div className="card">
-    <div className="card-body">
+    <div className="card-body d-flex flex-column">
       <a href={url} className="card-title stretched-link text-decoration-none fw-bold fs-5">{acronym ? `${title} (${acronym})` : title}</a>
       <p className="card-text">
         {degrees.map(degree => (
@@ -12,6 +12,7 @@ export const ProgramCard = ({ title, acronym, url = "#", degrees = [], types = [
           </span>
         ))}
       </p>
+      <span className="d-block program-tags">{tags.map(tag => toTitleCase(tag)).join(", ")}</span>
     </div>
 
     <div className="card-footer text-muted bg-info bg-opacity-10">{types.join(", ")}</div>
