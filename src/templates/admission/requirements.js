9d5588aaff3fba99bd60808deb1c0a6baec74dc3
Raza Mahmood <razammahmood@gmail.com>
2023-12-04 15:22:58 -0500

    update template styling and add headings

--- src/templates/admission/requirements.js
+++ src/templates/admission/requirements.js
@@ -7,33 +7,41 @@ export default function UndergraduateRequirements({ data, children, pageContext 
   const parents = data.parents.nodes
 
   const renderMainContent = content => (
-    <div className="mt-4" dangerouslySetInnerHTML={{ __html: main?.content?.[content] }}></div>
+    <div className="mt-5" dangerouslySetInnerHTML={{ __html: main?.content?.[content] }}></div>
   )
 
   const renderParentContent = content => {
     return parents.map(parent => (
-      <div className="mt-4" key={parent.slug} dangerouslySetInnerHTML={{ __html: parent?.content?.[content] }}></div>
+      <div className="mt-5" key={parent.slug} dangerouslySetInnerHTML={{ __html: parent?.content?.[content] }}></div>
     ))
   }
 
   return (
-    <Container className="mt-4">
+    <Container className="my-5">
       <h1 className="my-5">{`Admission Requirements for ${main.title}`}</h1>
 
       <Tabs defaultActiveKey="requirements" id="admission-requirements-tabs" justify>
-        <Tab eventKey="requirements" title="Requirements">
+        <Tab eventKey="requirements" title="Academic Requirements">
+          <h2 className="mt-5">Academic Requirements</h2>
+
           {renderParentContent("requirements")}
           {renderMainContent("requirements")}
         </Tab>
         <Tab eventKey="before-applying" title="Before Applying">
+          <h2 className="mt-5">Before Applying</h2>
+
           {renderParentContent("before_applying")}
           {renderMainContent("before_applying")}
         </Tab>
         <Tab eventKey="how-to-apply" title="How to Apply">
+          <h2 className="mt-5">How to Apply</h2>
+
           {renderParentContent("how_to_apply")}
           {renderMainContent("how_to_apply")}
         </Tab>
         <Tab eventKey="after-applying" title="After Applying">
+          <h2 className="mt-5">After Applying</h2>
+
           {renderParentContent("after_applying")}
           {renderMainContent("after_applying")}
         </Tab>
