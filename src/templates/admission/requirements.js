3d500a788f545cfe058bee0ef97d9ebfed0fe1f9
Raza Mahmood <razammahmood@gmail.com>
2023-12-04 13:39:38 -0500

    update template to fix tabs

--- src/templates/admission/requirements.js
+++ src/templates/admission/requirements.js
@@ -1,83 +1,43 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { graphql } from "gatsby"
-import { Container } from "react-bootstrap"
+import { Container, Tab, Tabs } from "react-bootstrap"
 
 export default function UndergraduateRequirements({ data, children, pageContext }) {
   const main = data.main.nodes[0]
   const parents = data.parents.nodes
 
+  const renderMainContent = content => (
+    <div className="mt-4" dangerouslySetInnerHTML={{ __html: main?.content?.[content] }}></div>
+  )
+
+  const renderParentContent = content => {
+    return parents.map(parent => (
+      <div className="mt-4" key={parent.slug} dangerouslySetInnerHTML={{ __html: parent?.content?.[content] }}></div>
+    ))
+  }
+
   return (
     <Container className="mt-4">
       <h1 className="my-5">{`Admission Requirements for ${main.title}`}</h1>
-      <ul className="nav nav-tabs nav-fill" id="admission-requirements-tabs">
-        <li className="nav-item" role="presentation">
-          <button className="nav-link active" id="requirements-tab" data-bs-toggle="tab" data-bs-target="#requirements">
-            Admission Requirements
-          </button>
-        </li>
-        <li className="nav-item" role="presentation">
-          <button className="nav-link" id="before-applying-tab" data-bs-toggle="tab" data-bs-target="#before-applying">
-            Before Applying
-          </button>
-        </li>
-        <li className="nav-item" role="presentation">
-          <button className="nav-link" id="how-to-apply-tab" data-bs-toggle="tab" data-bs-target="#how-to-apply">
-            How to Apply
-          </button>
-        </li>
-        <li className="nav-item" role="presentation">
-          <button className="nav-link" id="after-applying-tab" data-bs-toggle="tab" data-bs-target="#after-applying">
-            Applying
-          </button>
-        </li>
-      </ul>
 
-      <div className="tab-content">
-        <div className="tab-pane active" id="requirements" role="tabpanel" aria-labelledby="home-tab">
-          {parents.map(parent => (
-            <div
-              className="mt-4"
-              key={parent.slug}
-              dangerouslySetInnerHTML={{ __html: parent?.content?.requirements }}
-            ></div>
-          ))}
-
-          <div className="mt-4" dangerouslySetInnerHTML={{ __html: main?.content?.requirements }}></div>
-        </div>
-        <div className="tab-pane" id="before-applying" role="tabpanel" aria-labelledby="before-applying-tab">
-          {parents.map(parent => (
-            <div
-              className="mt-4"
-              key={parent.slug}
-              dangerouslySetInnerHTML={{ __html: parent?.content?.before_applying }}
-            ></div>
-          ))}
-
-          <div className="mt-4" dangerouslySetInnerHTML={{ __html: main?.content?.before_applying }}></div>
-        </div>
-        <div className="tab-pane" id="how-to-apply" role="tabpanel" aria-labelledby="how-to-apply-tab">
-          {parents.map(parent => (
-            <div
-              className="mt-4"
-              key={parent.slug}
-              dangerouslySetInnerHTML={{ __html: parent?.content?.how_to_apply }}
-            ></div>
-          ))}
-
-          <div className="mt-4" dangerouslySetInnerHTML={{ __html: main?.content?.how_to_apply }}></div>
-        </div>
-        <div className="tab-pane" id="after-applying" role="tabpanel" aria-labelledby="after-applying-tab">
-          {parents.map(parent => (
-            <div
-              className="mt-4"
-              key={parent.slug}
-              dangerouslySetInnerHTML={{ __html: parent?.content?.after_applying }}
-            ></div>
-          ))}
-
-          <div className="mt-4" dangerouslySetInnerHTML={{ __html: main?.content?.after_applying }}></div>
-        </div>
-      </div>
+      <Tabs defaultActiveKey="requirements" id="admission-requirements-tabs" justify>
+        <Tab eventKey="requirements" title="Requirements">
+          {renderParentContent("requirements")}
+          {renderMainContent("requirements")}
+        </Tab>
+        <Tab eventKey="before-applying" title="Before Applying">
+          {renderParentContent("before_applying")}
+          {renderMainContent("before_applying")}
+        </Tab>
+        <Tab eventKey="how-to-apply" title="How to Apply">
+          {renderParentContent("how_to_apply")}
+          {renderMainContent("how_to_apply")}
+        </Tab>
+        <Tab eventKey="after-applying" title="After Applying">
+          {renderParentContent("after_applying")}
+          {renderMainContent("after_applying")}
+        </Tab>
+      </Tabs>
     </Container>
   )
 }
