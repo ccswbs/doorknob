de73410b59798b78003d28fbc358601af0bb8d69
Raza Mahmood <razammahmood@gmail.com>
2023-12-06 15:39:46 -0500

    working on requirements filter page

---
+++ src/utils/to-title-case.js
@@ -0,0 +1,23 @@
+export const toTitleCase = str => {
+  const lowercaseWords = [
+    "a",
+    "an",
+    "the",
+    "and",
+    "but",
+    "or",
+    "for",
+    "nor",
+    "on",
+    "at",
+    "to",
+    "by",
+    "of",
+    "in",
+    "with",
+  ]
+
+  return str.toLowerCase().replace(/\w+(?:-\w+)*/g, word => {
+    return lowercaseWords.includes(word) ? word : word.charAt(0).toUpperCase() + word.slice(1)
+  })
+}
