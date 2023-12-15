7eb6b243df6139555ec63dc1bcf1c5886eceb7f7
Raza Mahmood <razammahmood@gmail.com>
2023-12-15 11:13:15 -0500

    Revert "abandon idea, go back to slug (revisit idea later)"
    This reverts commit 11ce975f5ba911e545ba7cab0a9ee56c1fd41811.

--- src/templates/admission/requirements.js
+++ src/templates/admission/requirements.js
@@ -3,37 +3,72 @@ import { graphql } from "gatsby"
 import { Container, Tab, Tabs } from "react-bootstrap"
 
 export default function Requirements({ data, children, pageContext }) {
-  const requirements = data.requirements.nodes
+  const main = data.main.nodes[0]
+  const parents = data.parents.nodes
 
-  console.log(requirements)
+  const renderMainContent = content => (
+    <div className="mt-5" dangerouslySetInnerHTML={{ __html: main?.content?.[content] }}></div>
+  )
+
+  const renderParentContent = content => {
+    return parents.map(parent => (
+      <div className="mt-5" key={parent.slug} dangerouslySetInnerHTML={{ __html: parent?.content?.[content] }}></div>
+    ))
+  }
 
   return (
     <Container className="my-5">
-      <h1 className="my-5">{`Admission Requirements for`}</h1>
+      <h1 className="my-5">{`Admission Requirements for ${main.title}`}</h1>
 
       <Tabs defaultActiveKey="requirements" id="admission-requirements-tabs" justify>
         <Tab eventKey="requirements" title="Academic Requirements">
           <h2 className="mt-5">Academic Requirements</h2>
+
+          {renderParentContent("requirements")}
+          {renderMainContent("requirements")}
         </Tab>
         <Tab eventKey="before-applying" title="Before Applying">
           <h2 className="mt-5">Before Applying</h2>
+
+          {renderParentContent("before_applying")}
+          {renderMainContent("before_applying")}
         </Tab>
         <Tab eventKey="how-to-apply" title="How to Apply">
           <h2 className="mt-5">How to Apply</h2>
+
+          {renderParentContent("how_to_apply")}
+          {renderMainContent("how_to_apply")}
         </Tab>
         <Tab eventKey="after-applying" title="After Applying">
           <h2 className="mt-5">After Applying</h2>
+
+          {renderParentContent("after_applying")}
+          {renderMainContent("after_applying")}
         </Tab>
       </Tabs>
     </Container>
   )
 }
 
+/*
 export const query = graphql`
-  query ($requirements: [String!]!) {
-    requirements: allAdmissionRequirementsYaml(filter: { id: { in: $requirements } }) {
+  query ($slug: String!, $parents: [String!]!) {
+    main: allAdmissionRequirementsYaml(filter: { slug: { eq: $slug } }) {
       nodes {
-        id
+        slug
+        title
+        content {
+          requirements
+          before_applying
+          how_to_apply
+          after_applying
+        }
+      }
+    }
+    parents: allAdmissionRequirementsYaml(filter: { slug: { in: $parents } }, sort: { slug: DESC }) {
+      nodes {
+        slug
+        title
         content {
           requirements
           before_applying
@@ -44,3 +79,4 @@ export const query = graphql`
     }
   }
 `
+*/
