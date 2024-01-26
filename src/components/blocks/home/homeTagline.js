c5b525e77fb3a60ecc453a7de8aa5489f29ac94e
Miranda Mafe <miranda.mafe@gmail.com>
2024-01-26 12:12:28 -0500

    Visual change test

--- src/components/blocks/home/homeTagline.js
+++ src/components/blocks/home/homeTagline.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const HomeTagline = () => (
   <div className="container text-center d-flex flex-column home-tagline">
-    <h2 className="mt-5 mb-4">University of Guelph, Ontario, Canada</h2>
+    <h2 className="mt-5 mb-4">University of Guelph - Ontario, Canada</h2>
     <p className="lead text-dark ps-4 display-1 fs-4 mx-auto">Improve Life</p>
     <hr className="w-75 mx-auto"></hr>
   </div>
