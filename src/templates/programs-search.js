a99208500aabc89d1da398bf198bc1e5cc38517c
Raza Mahmood <razammahmood@gmail.com>
2024-02-08 14:57:50 -0500

    seperate filtering logic from component

--- src/templates/programs-search.js
+++ src/templates/programs-search.js
@@ -1,12 +1,69 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { graphql } from "gatsby";
 import { Container } from "react-bootstrap";
 import { LinkTabs } from "../components/linkTabs.js";
 import { toTitleCase } from "../utils/toTitleCase.js";
-import { ProgramSearch } from "../components/blocks/programs/programSearch.js";
-import "../styles/program-search.scss"
+import { useSearch } from "../utils/use-search.js";
+import "../styles/program-search.scss";
+
+export const ProgramCard = ({ title, acronym, url = "#", degrees = [], types = [], tags = [] }) => (
+  <div className="card">
+    <div className="card-body d-flex flex-column">
+      <a href={url} className="card-title stretched-link text-decoration-none fw-bold fs-5">
+        {acronym ? `${title} (${acronym})` : title}
+      </a>
+      <p className="card-text">
+        {degrees.map(degree => (
+          <span className="d-block" key={degree}>
+            {degree}
+          </span>
+        ))}
+      </p>
+      <span className="d-block program-tags">
+        {tags
+          .map(tag => toTitleCase(tag))
+          .sort((a, b) => a.localeCompare(b))
+          .join(", ")}
+      </span>
+    </div>
+
+    <div className="card-footer text-muted bg-info bg-opacity-10">{types.join(", ")}</div>
+  </div>
+);
+
+export const ProgramSearch = ({ data }) => {
+  const [input, setInput] = useState("");
+  const filtered = useSearch(data, input);
+
+  return (
+    <Container>
+      <div className="row gap-5 gap-lg-0">
+        <div className="col-lg-9">
+          <label htmlFor="program-search-input" className="form-label">
+            What would you like to study?
+          </label>
+          <input
+            id="program-search-input"
+            className="form-control form-control-md"
+            type="text"
+            onChange={e => setInput(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <div id="program-search-grid" className="my-5">
+        {filtered.map(program => (
+          <ProgramCard key={program.id} {...program} />
+        ))}
+      </div>
+    </Container>
+  );
+};
 
 export default function ProgramSearchTemplate({ data, children, pageContext }) {
+  const [input, setInput] = useState("");
+  const filtered = useSearch(data.programs.nodes, input);
+
   return (
     <>
       <Container className="my-5">
@@ -17,7 +74,27 @@ export default function ProgramSearchTemplate({ data, children, pageContext }) {
         />
       </Container>
 
-      <ProgramSearch data={data.programs.nodes} />
+      <Container>
+        <div className="row gap-5 gap-lg-0">
+          <div className="col-lg-9">
+            <label htmlFor="program-search-input" className="form-label">
+              What would you like to study?
+            </label>
+            <input
+              id="program-search-input"
+              className="form-control form-control-md"
+              type="text"
+              onChange={e => setInput(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div id="program-search-grid" className="my-5">
+          {filtered.map(program => (
+            <ProgramCard key={program.id} {...program} />
+          ))}
+        </div>
+      </Container>
     </>
   );
 }
---
+++ src/utils/use-search.js
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+
+const pattern = /\s+|\band\b|\bof\b|\bin\b/g;
+
+const defaultRanker = (node, keywords) => {
+  // Split some of the program props into an array of words just like the keywords.
+  const title = node?.title?.toLowerCase().split(pattern) || [];
+  const tags = node?.tags?.map(tag => tag.toLowerCase()) || [];
+
+  // We rank each keyword individually, then take the highest (lowest number) rank as the program's rank overall.
+  // If any single keyword doesn't match (i.e. rank = -1), then the program as a whole doesn't match.
+  return keywords.reduce((rank, keyword) => {
+    // The previous keyword didn't match, so this keyword can't match either, so we don't need to get its rank.
+    if (rank === -1) return -1;
+
+    // If any word in the title starts with or is equal to the keyword return 0 (highest rank).
+    if (title.some(word => word.startsWith(keyword))) return 0;
+
+    // If the keyword is in the program's tags, return 2 if the none of the previous keywords had a higher rank.
+    if (tags.some(tag => tag.startsWith(keyword))) return isNaN(rank) ? 2 : Math.min(rank, 1);
+
+    return -1;
+  }, NaN);
+};
+
+const defaultParser = input => {
+  return (
+    input
+      .toLowerCase()
+      // Remove leading and trailing whitespace
+      .trim()
+      // Remove all non-alphabetic and non-whitespace characters
+      .replace(/[^a-zA-Z\s]/g, "")
+      // Split on whitespace and "and"
+      .split(pattern)
+      // Remove empty strings
+      .filter(word => word.length > 0)
+  );
+};
+
+const defaultSorter = (a, b) => a?.title?.localeCompare(b?.title);
+
+export const useSearch = (data, input, ranker = defaultRanker, parser = defaultParser, sorter = defaultSorter) => {
+  if (!Array.isArray(data)) {
+    throw new TypeError("data must be an array");
+  }
+
+  const [filtered, setFiltered] = useState(data);
+
+  useEffect(() => {
+    if (!input) {
+      setFiltered(data);
+      return;
+    }
+
+    const keywords = parser(input);
+    const filteredData = data
+      ?.map(element => ({ data: element, rank: ranker(element, keywords) }))
+      ?.filter(node => node.rank >= 0)
+      ?.sort((a, b) => {
+        // Sort by rank
+        if (a.rank < b.rank) return -1;
+        if (a.rank > b.rank) return 1;
+
+        return sorter(a, b);
+      })
+      ?.map(node => node.data);
+
+    setFiltered(filteredData || []);
+  }, [input, ranker, parser]);
+
+  return filtered;
+};
