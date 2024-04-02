21ca46b84dd937a80d02060088acab6f21b5c56c
Raza Mahmood <razammahmood@gmail.com>
2024-04-02 14:03:59 -0400

    add filter by type to undergraduate.js

--- src/pages/programs/undergraduate.js
+++ src/pages/programs/undergraduate.js
@@ -3,18 +3,20 @@ import { LinkTabs } from "../../components/linkTabs.js";
 import { Form, Container } from "react-bootstrap";
 import ProgramSearch from "../../components/blocks/programs/programs-search.js";
 import { graphql } from "gatsby";
+import { toTitleCase } from "../../utils/string-utils.js";
 
 export default function ProgramsUndergraduate({ data, children }) {
-  const [onlyMajors, setOnlyMajors] = useState(false);
   const [onlyCoop, setOnlyCoop] = useState(false);
+  const types = Array.from(new Set(data.programs.nodes.flatMap(program => program.types)));
+  const [type, setType] = useState("any");
 
   const filterer = useMemo(
     () => program => {
-      if (onlyMajors && !program.types.some(type => type.toLowerCase() === "major")) return false;
+      if (type !== "any" && !program.types.includes(type)) return false;
       if (onlyCoop && !program.types.some(type => type.toLowerCase() === "co-op")) return false;
       return true;
     },
-    [onlyMajors, onlyCoop],
+    [onlyCoop, type],
   );
 
   return (
@@ -44,25 +46,36 @@ export default function ProgramsUndergraduate({ data, children }) {
         />
       </Container>
 
-      <ProgramSearch programs={data.programs.nodes} filterer={filterer}>
-        {
+      <ProgramSearch
+        programs={data.programs.nodes}
+        sidebar={
           <>
-            <Form.Check
-              type="checkbox"
-              id="program-search-majors-checkbox"
-              label="Only show programs offered as a major"
-              className="mt-2"
-              onChange={e => setOnlyMajors(e.target.checked)}
-            />
+            <Form.Label htmlFor="program-search-undergraduate-type">Filter by type</Form.Label>
 
-            <Form.Check
-              type="checkbox"
-              id="program-search-co-op-checkbox"
-              label="Only show programs offering co-op"
-              className="mt-2"
-              onChange={e => setOnlyCoop(e.target.checked)}
-            />
+            <Form.Select
+              id="program-search-undergraduate-type"
+              aria-label="Default select example"
+              onChange={e => {
+                setType(e.target.value);
+              }}
+            >
+              <option value="any">Any</option>
+              <option value="major">Major</option>
+              <option value="minor">Minor</option>
+              <option value="area-of-concentration">Area of Concentration</option>
+            </Form.Select>
           </>
+        }
+        filterer={filterer}
+      >
+        {
+          <Form.Check
+            type="checkbox"
+            id="program-search-co-op-checkbox"
+            label="Only show programs offering co-op"
+            className="mt-2"
+            onChange={e => setOnlyCoop(e.target.checked)}
+          />
         }
       </ProgramSearch>
     </>
