74f1cd22bb36984956e32b72f822ba09111bdd08
Raza Mahmood <razammahmood@gmail.com>
2024-04-04 13:38:58 -0400

    revamp rank function

--- src/hooks/use-search.js
+++ src/hooks/use-search.js
@@ -15,88 +15,98 @@ const parse = input =>
     .filter(word => word.length > 0);
 
 const rank = (node, parsed) => {
-  const RANKS = {
+  const MATCH_TYPES = {
     EXACT: 10,
     CLOSE: 8,
     MATCHING_STEM: 6,
     STARTS_WITH: 4,
+    MATCHES_WILDCARD: 5,
+    NONE: 0,
   };
 
-  const ranker = (a, b) => {
-    // If the keyword and the word are the same.
-    // We test the length first to avoid the more expensive string comparison as it's not necessary if the lengths are different.
+  const isMatch = (a, b) => {
+    // Matches when A and B are the same. We test the length first to avoid the more expensive string comparison as it's not necessary if the lengths are different.
     if (a.length === b.length && a === b) {
-      // We check how close the keyword is to the word in the user input. This helps in situations where the user may have typed something like "biology", and we have two results "biology" and "animal biology". We want to rank "biology" higher than "animal biology" in this case as it's a closer match.
-      return RANKS.EXACT;
+      // We check how close A is to the B. This helps in situations where the user may have typed something like "biology", and we have two results "biology" and "animal biology". We want to rank "biology" higher than "animal biology" in this case as it's a closer match.
+      return MATCH_TYPES.EXACT;
     }
 
-    // If the keyword and the word are within 2 characters of each other and have an edit distance of 2 or less (i.e. 2 or fewer changes), add 7 to the rank. This is to account for typos and minor spelling mistakes. We test the length difference first to avoid the more expensive editDistance function as it's not necessary if the length difference is too large.
+    // Matches when A and B are within 2 characters of each other and have an edit distance of 2 or less (i.e. 2 or fewer changes). This is to account for typos and minor spelling mistakes. We test the length difference first to avoid the more expensive editDistance function as it's not necessary if the length difference is too large.
     if (Math.abs(a.length - b.length) < 2 && editDistance(a, b) < 2) {
-      return RANKS.CLOSE;
+      return MATCH_TYPES.CLOSE;
     }
 
-    // Stemming (the process of finding the root of a word ex. computer -> comput) can help with matching words that are similar but not exactly the same. For example, "computer" and "computing" are related words, and we may want to match them. We use the Porter stemming algorithm to achieve this. Since stemming is an expensive operation, we only do it if the first 3 characters of the keyword and the word are the same as words that don't match at least the first 3 characters will not have the same stem.
+    // Stemming (the process of finding the root of a word ex. computer -> comput) can help with matching words that are similar but not exactly the same. For example, "computer" and "computing" are related words, and we may want to match them. We use the Porter stemming algorithm to achieve this. Since stemming is an expensive operation, we only do it if the first 3 characters of A and B are the same, as words that don't match at least the first 3 characters will not have the same stem.
     if (strncmp(a, b, 3) && stemmer(a) === stemmer(b)) {
-      return RANKS.MATCHING_STEM;
+      return MATCH_TYPES.MATCHING_STEM;
     }
 
-    // If the keyword starts with the word. This is to account for the user typing the beginning of a word.
+    // If A starts with B. This is to account for the user typing the beginning of a word.
     if (a.startsWith(b)) {
-      return RANKS.STARTS_WITH;
+      return MATCH_TYPES.STARTS_WITH;
     }
 
-    return 0;
+    return MATCH_TYPES.NONE;
   };
 
-  let rank = 0;
+  const matches = new Map();
 
-  for (let i = 0; i < parsed.length; i++) {
-    const word = parsed[i];
-    const previousRank = rank;
-
-    // If the previous word in the user input didn't match then we can skip this node as we don't want partial matches. This avoids matching things like "animal science" when the user types "computer science".
-    if (rank === 0 && i !== 0) {
-      return 0;
-    }
+  main: for (let i = 0; i < parsed.length; i++) {
+    const parsedWord = parsed[i];
 
     for (let j = 0; j < node.keywords.length; j++) {
-      // Get the rank but subtract the distance between the keyword and the word in the user input. This is to show results that are closer to the word in the user input first. For example, if the user types "Economics" we want to show "Economics" before "Business Economics" even though "Business Economics" has the same rank.
-      rank += Math.max(ranker(node.keywords[j], word) - Math.min(Math.abs(i - j), 1), 0);
-    }
+      const keyword = node.keywords[j];
+      const match = isMatch(keyword, parsedWord);
 
-    // If the rank has changed, that means the word was found in the keywords, and we don't need to check the tags.
-    if (previousRank !== rank) {
-      continue;
-    }
+      if (match !== MATCH_TYPES.NONE) {
+        matches.set(parsedWord, {
+          type: match,
+          isKeyword: true,
+          distance: Math.abs(i - j),
+          word: keyword,
+        });
 
-    if (node.tags) {
-      for (const tag of node.tags) {
-        if (Array.isArray(tag)) {
-          // Process multi-word tags here
-        } else {
-          const partialRank = ranker(tag, word);
-
-          if (partialRank > 0) {
-            // Tags are less important than keywords, so we divide the rank by 2 to give them less weight.
-            rank += partialRank / 2;
-            continue;
-          }
-
-          // Wildcard tags
-          if (tag.length - 1 < word.length && tag.endsWith("*") && word.startsWith(tag.slice(0, -1))) {
-            rank += RANKS.EXACT / 2;
-          }
-        }
+        // If the parsed word matched a keyword, we don't need to bother checking the tags. We move on to the next parsed word.
+        continue main;
       }
     }
 
-    // If after processing all the keywords and tags the rank hasn't changed, that means the word wasn't found in the keywords or tags, and we can skip this node. This avoids matching things like "animal science" when the user types "science computer".
-    if (previousRank === rank) {
-      return 0;
+    for (let j = 0; j < node.tags.length; j++) {
+      const tag = node.tags[j];
+
+      if (Array.isArray(tag)) {
+      } else {
+        let match = isMatch(tag, parsedWord);
+
+        if (match === MATCH_TYPES.NONE) {
+          // Check wildcard matches
+          if (tag.length - 1 < parsedWord.length && tag.endsWith("*") && parsedWord.startsWith(tag.slice(0, -1))) {
+            match = MATCH_TYPES.MATCHES_WILDCARD;
+          }
+        }
+
+        if (match !== MATCH_TYPES.NONE) {
+          matches.set(parsedWord, {
+            type: match,
+            isKeyword: false,
+            distance: Math.abs(i - j),
+            word: tag,
+          });
+        }
+      }
     }
   }
 
-  return rank;
+  // If we didn't find a match for every parsed word, we return 0.
+  if (matches.size !== parsed.length) {
+    return 0;
+  }
+
+  // We calculate the rank by summing the type of each match. We give more weight to keyword matches than tag matches.
+  return Array.from(matches.values()).reduce(
+    (acc, match) => (match.isKeyword ? acc + match.type - Math.min(match.distance, 1) : acc + match.type / 2),
+    0,
+  );
 };
 
 export function defaultSearchFunc(data) {
