02793fac2c393c48d79b23434e5df80c466beaa3
Raza Mahmood <razammahmood@gmail.com>
2024-05-14 13:03:51 -0400

    add replaceHydrateFunction

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,14 @@
 import React from "react"
+import ReactDOM from 'react-dom'
 import Layout from "./src/components/layout"
 
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
+}
+
+export const replaceHydrateFunction = () => {
+  return (element, container) => {
+    const root = ReactDOM.createRoot(container)
+    root.render(element)
+  }
 }
