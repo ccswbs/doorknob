84de9139d4e285e6973518e86d847ff289b14275
Raza Mahmood <razammahmood@gmail.com>
2024-05-14 13:07:42 -0400

    react-dom/client instead of react-dom

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,5 @@
 import React from "react"
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import Layout from "./src/components/layout"
 
 export const wrapPageElement = ({ element, props }) => {
